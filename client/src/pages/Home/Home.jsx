4e66a61e94c086f5b46ef92af7e8bbcbb4784a4f
Mritul <mritul2003@gmail.com>
2022-08-20 22:30:26 +0530

    Go to admin link added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Link to="/admin"> go to admin</Link>
     </div>
   );
 };
